80ebe30670708edf183b75d3e345d3c1afd86153
Harishsbb <bavaharishkumar@gmail.com>
2026-08-18 21:56:31 +0530

    encapsulation added for user account

--- src/main/java/com/bank/entity/User.java
+++ src/main/java/com/bank/entity/User.java
@@ -1,5 +1,54 @@
 package com.bank.entity;
+import jakarta.persistence.*;
 
+@Entity
+@table(name="user")
 public class User {
-    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String username;
+    private String emailID;
+    private String password;
+    private String role;
+
+    public user(){
+
+    }
+
+    public Long getId(){
+        return id;
+    }
+
+    public String getUsername(){
+        return username;
+    }
+
+    public void setUsername(String username){
+        this.username=username;
+    }
+
+    public String getEmail() {
+        return emailID;
+    }
+
+    public void setEmail(String emailID) {
+        this.emailID = emailID;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
 }
